182888e6f896a9b6cd3b263a701209dbc9d41ac6
Varvara <varvara.sharova@tum.de>
2024-11-28 13:38:53 +0100

    i changed the design

--- projectwork2024/projectwork2024/urls.py
+++ projectwork2024/projectwork2024/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from projectwork2024 import views  # Импортируем views из приложения
+from projectwork2024 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', views.chat_interface, name='chat'),  # Маршрут для чата
-    path('', views.chat_interface, name='home'),  # Главная страница (может использоваться тот же view, что и для чата)
+    path('chat/', views.chat_interface, name='chat'),
+    path('', views.chat_interface, name='home'),
 ]
--- projectwork2024/projectwork2024/views.py
+++ projectwork2024/projectwork2024/views.py
@@ -5,6 +5,6 @@ from .models import UserInput
 import json
 
 def chat_interface(request):
-    question = request.GET.get('question', '')  # Получаем вопрос из параметров запроса (если есть)
-    context = {'question': question}  # Передаём вопрос в шаблон
-    return render(request, 'chat.html', context)  # Рендерим шаблон chat.html
+    question = request.GET.get('question', '')
+    context = {'question': question}
+    return render(request, 'chat.html', context)
